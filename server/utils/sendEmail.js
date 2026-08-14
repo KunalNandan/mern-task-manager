cc2f46ea0e596e2c5cb86febb450981625cf2ace
KunalNandan <kunalnandan695@gmail.com>
2026-08-14 12:50:02 +0530

    Switch password reset email to Gmail SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,21 +1,29 @@
-const { Resend } = require("resend");
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASSWORD,
+    },
+});
 
 const sendEmail = async (to, subject, html) => {
-    const { data, error } = await resend.emails.send({
-        from: "Task Manager <onboarding@resend.dev>",
-        to: [to],
-        subject,
-        html,
-    });
+    try {
+        const info = await transporter.sendMail({
+            from: `Task Manager <${process.env.GMAIL_USER}>`,
+            to,
+            subject,
+            html,
+        });
 
-    if (error) {
-        console.error("Resend email error:", error);
-        throw new Error(error.message);
+        console.log("Email sent:", info.messageId);
+
+        return info;
+    } catch (error) {
+        console.error("Gmail email error:", error);
+        throw new Error("Failed to send email");
     }
-
-    return data;
 };
 
 module.exports = sendEmail;
